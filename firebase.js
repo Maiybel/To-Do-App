be0d3e7c40ff5439705b0eaddf797eb7a61f0290
Ezema Mabel <ezemamabel57@gmail.com>
2025-03-20 16:06:39 +0100

    improve functionality of to do app

--- firebase.js
+++ firebase.js
@@ -1,49 +1,15 @@
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyAWcpTB0gCcSdjGzF13Qs5-tbOto5N4qYg",
-// //   authDomain: "todo-live-b94b8.firebaseapp.com",
-// //   projectId: "todo-live-b94b8",
-// //   storageBucket: "todo-live-b94b8.firebasestorage.app",
-// //   messagingSenderId: "820598963315",
-// //   appId: "1:820598963315:web:46bbd949dff3fd5c697e06",
-// //   measurementId: "G-DS99SWRW7E"
-// // };
+const firebaseConfig = {
+  apiKey: "AIzaSyBmEuZmzHtsyXzOQhSWUGPu6c3uRUJC6VY",
+  authDomain: "todo-app-new-3e900.firebaseapp.com",
+  projectId: "todo-app-new-3e900",
+  storageBucket: "todo-app-new-3e900.firebasestorage.app",
+  messagingSenderId: "264571526135",
+  appId: "1:264571526135:web:260c5314ab20234cd36cf2",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// // // Initialize Firebase
-// // firebase.initializeApp(firebaseConfig);
+// Initialize Cloud Firestore
+const db = firebase.firestore();
 
-// // // Initialize Cloud Firestore and get a reference to the service
-// // const db = firebase.firestore();
-
-// // Firebase configuration
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyAWcpTB0gCcSdjGzF13Qs5-tbOto5N4qYg",
-// //   authDomain: "todo-live-b94b8.firebaseapp.com",
-// //   projectId: "todo-live-b94b8",
-// //   storageBucket: "todo-live-b94b8.firebasestorage.app",
-// //   messagingSenderId: "820598963315",
-// //   appId: "1:820598963315:web:46bbd949dff3fd5c697e06",
-// //   measurementId: "G-DS99SWRW7E",
-// // };
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBmEuZmzHtsyXzOQhSWUGPu6c3uRUJC6VY",
-//   authDomain: "todo-app-new-3e900.firebaseapp.com",
-//   projectId: "todo-app-new-3e900",
-//   storageBucket: "todo-app-new-3e900.firebasestorage.app",
-//   messagingSenderId: "264571526135",
-//   appId: "1:264571526135:web:260c5314ab20234cd36cf2",
-// };
-
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
-
-// // Initialize Cloud Firestore
-// const db = firebase.firestore();
-
-// console.log("Firebase initialized successfully");
+console.log("Firebase initialized successfully");
